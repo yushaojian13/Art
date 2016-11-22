038e3f73d53b91bcbcdeab7ba2d0f564d829fda4
yushaojian <yushaojian13@163.com>
2016-11-22 15:21:46 +0800

    glide:xxx-integration study

--- app/src/main/java/com/ysj/art/MainActivity.java
+++ app/src/main/java/com/ysj/art/MainActivity.java
@@ -1,7 +1,10 @@
 package com.ysj.art;
 
-import android.support.v7.app.AppCompatActivity;
+import com.bumptech.glide.Glide;
+
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ImageView demoIV = (ImageView) findViewById(R.id.demoIV);
+        Glide.with(this).load("http://ww4.sinaimg.cn/large/610dc034gw1fa0ppsw0a7j20u00u0thp.jpg").into(demoIV);
     }
 }
